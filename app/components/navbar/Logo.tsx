8203808b122aeaa548dd1e8d42daddfb640eee50
riumat <matteoariu42@gmail.com>
2024-06-28 17:38:21 +0200

    init routes pages

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,4 +1,4 @@
-import { Roboto_Mono as Font } from "next/font/google"
+import { Grandstander as Font } from "next/font/google"
 import FastfoodSharpIcon from '@mui/icons-material/FastfoodSharp';
 
 const font = Font({ weight: "400", subsets: ["latin"] })
@@ -6,8 +6,8 @@ const font = Font({ weight: "400", subsets: ["latin"] })
 const Logo = () => {
   return (
     <div className={`${font.className} flex  items-center gap-3 flex-1 justify-center`}>
-      <FastfoodSharpIcon className=" text-[25px]" />
-      <p className="text-[20px] ">Abileroo</p>
+      <FastfoodSharpIcon className=" text-[25px] " />
+      <p className="text-[22px]  h-6 ">Abileroo</p>
     </div>
   )
 }
--- app/components/navbar/NavbarLinks.tsx
+++ app/components/navbar/NavbarLinks.tsx
@@ -6,7 +6,6 @@ import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import RestaurantOutlinedIcon from '@mui/icons-material/RestaurantOutlined';
 import LunchDiningOutlinedIcon from '@mui/icons-material/LunchDiningOutlined';
 import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
-import { c } from 'tar';
 
 const navLinks = [
   { title: "Home", path: "/", icon: <HomeOutlinedIcon /> },
@@ -14,7 +13,6 @@ const navLinks = [
   { title: "Products", path: "/products", icon: <LunchDiningOutlinedIcon className='w-5 h-5' /> },
   { title: "Orders", path: "/orders", icon: <MenuBookOutlinedIcon className='w-5 h-5' /> },
 ]
-
 
 const NavbarLinks = () => {
   const pathname = usePathname();
@@ -24,7 +22,7 @@ const NavbarLinks = () => {
         <Link
           href={link.path}
           key={`navlink-${index}`}
-          className={`border-b-2 flex gap-1 items-center p-1 ${pathname === link.path ? "border-orange-950" : "border-neutral-300"}`}
+          className={`border-b-[2.5px] flex gap-1 items-center p-1 ${pathname === link.path ? "border-orange-900" : "border-neutral-300"}`}
         >
           {link.icon}
           <p className='text-[14px] font-semibold'>{link.title}</p>
